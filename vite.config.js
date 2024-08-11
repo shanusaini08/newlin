127d006d38f2bf1acfd08e0037f3bac685ecf13d
arcs-success <shanu.saini.vns@gmail.com>
2024-08-12 01:05:48 +0530

    Completed all the pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/newlin/', // Adjust this to your repository name
+  base: '/newlin/', // Base path for GitHub Pages
 });

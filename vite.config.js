91971e30be57ce9e3aedbb55a277579dc83605c7
arcs-success <shanu.saini.vns@gmail.com>
2024-08-11 16:33:15 +0530

    Updated project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/newlin/', // Make sure this matches your GitHub repository name
+  base: '/', // Update this to your repository name
 });
